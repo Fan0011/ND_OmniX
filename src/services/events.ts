ecbb37612d6be93d512c27e2c772873d5f7f0ba8
Fan0011 <daisukehara742@gmail.com>
2022-07-27 10:06:38 -0700

    update events

--- src/services/events.ts
+++ src/services/events.ts
@@ -2,6 +2,7 @@
 import * as Test from "../abis/Test.json"
 import * as OmniXExchange from "../abis/OmniXExchange.json"
 import { ethers } from 'ethers'
+import OrdersController from '../controllers/ordersController'
 
 export const installBSCTestEvents = () => {
     var wsProvider = new ethers.providers.WebSocketProvider(process.env.BSCTEST_RPC as string)
@@ -24,11 +25,23 @@ export const installRopstenEvents = () => {
 export const installRinkebyEvents = () => {
     var wsProvider = new ethers.providers.WebSocketProvider(process.env.RINKEBY_RPC as string)
     let contract = new ethers.Contract("0x8405eA012aC6a3Ac998e42793e3275e011cf8E4e", OmniXExchange.abi, wsProvider)
+    console.log('listen now');
+    
+    const ordersController = new OrdersController()
 
     contract.on("CancelAllOrders", (from, to, value, event) => {
-        console.log(from)
-        console.log(to)
-        console.log(value)
-        console.log(event)
+        const args = value.args;
+        console.log('user: ', args.user);
+        console.log('newMinNonce: ', args.newMinNonce);
+    })
+
+    contract.on("TakerBid", (from, to, value, event) => {
+        const args = value.args;
+        ordersController.takerBid(args);
+    })
+
+    contract.on("TakerAsk", (from, to, value, event) => {
+        const args = value.args;
+        ordersController.takerBid(args);
     })
 }
